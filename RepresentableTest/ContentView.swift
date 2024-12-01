580ba9643276610df015a19e55788b4374884b7a
Eric Kampman <erickampman@yahoo.com>
2024-12-01 14:13:18 -0800

    Initial Commit

---
+++ RepresentableTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RepresentableTest
+//
+//  Created by Eric Kampman on 12/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RepresentableTest/RepresentableTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepresentableTestApp.swift
+//  RepresentableTest
+//
+//  Created by Eric Kampman on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct RepresentableTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
